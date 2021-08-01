71dc4940bd4385609cd3ad35455db8869af7b174
leau-void <lpierra@gmail.com>
2021-08-01 20:24:25 -0300

    done with unbeatable, ui left

--- scripts/script.js
+++ scripts/script.js
@@ -32,21 +32,112 @@ const PlayerFactory = (name, marker, isHuman, aiLevel) => {
     },
     
     pickCPU() {
-      //  console.log(aiLevel)
-     // if (this.aiLevel == "easy") {
+      let currentPick;
+      
+      
+     if (this.aiLevel == "easy") {
 
         let pickFunc = () => Math.floor(Math.random() * 9)
       
 
-        let currentPick = pickFunc();
+        currentPick = pickFunc();
 
         while(gameBoard.gameState[currentPick]) {
           currentPick = pickFunc();
         }
-        document.querySelector(`[data-position="${currentPick}"]`).click();
-
-     // }
+      } else if (this.aiLevel == "unbeatable") {
+        currentPick = this.findBestPick(gameBoard.gameState, this.picks);
+      }
       
+      document.querySelector(`[data-position="${currentPick}"]`).click();
+      
+    },
+    
+    findBestPick(state, picks) {
+
+      const otherPlayer = (this.marker == "O") ? game.playerOne : game.playerTwo;
+      let currentPick;
+      
+      if ((picks.length + otherPlayer.picks.length) == 8) {
+       return state.findIndex(position => position == null)
+      }
+      
+      const winOptions = [[0,1,2], [0,3,6], [0,4,8], [3,4,5], [1,4,7], [2,4,6], [6,7,8], [2,5,8]]
+ 
+      let closestWin = Array.from(winOptions).map(option => option.filter(position => !picks.includes(position))).sort((lastOption, nextOption) => {
+        return (lastOption.length < nextOption.length) ? -1 : 1
+      })
+        
+      closestWin = closestWin.filter(option => option.every(position => {
+        return (otherPlayer.picks.includes(position)) ? false : true;           
+      })).filter(option => option.length < 3)
+      if (closestWin.length && closestWin[0].length == 1) { 
+        return closestWin[0][0]
+      } else {
+        let closestOppWin = Array.from(winOptions).map(option => option.filter(position => !otherPlayer.picks.includes(position))).sort((lastOption, nextOption) => {
+          return (lastOption.length < nextOption.length) ? -1 : 1
+        })
+            
+      closestOppWin = closestOppWin.filter(option => option.every(position => {
+        return (picks.includes(position)) ? false : true;           
+      }))
+      if (closestOppWin[0] && closestOppWin[0].length == 1) { 
+        return closestOppWin[0][0]
+      }}
+
+      if (this.marker == "X") { 
+            if (this.roundsPlayed == 0) {
+            return 0;
+              
+            } else if (this.roundsPlayed == 1) {
+              return (state[4]) ? 8 : (game.playerTwo.picks.includes(1) || game.playerTwo.picks.includes(2)) ? 6 : 2;
+            } 
+            } else {
+            if (this.roundsPlayed== 0) {
+            return (state[4]) ? 0 : 4;
+            } else if (this.roundsPlayed == 3) {
+              return state.findIndex(position => position == null)
+            }
+      }
+
+      if (closestWin.length > 1) {
+        let output;
+        closestWin.forEach((option, i) => {
+          for (let j = 0; j < closestWin.length; j++) {
+            if (j == i) continue;
+            if (closestWin[j].includes(option[0])) {
+              output = option[0];
+              break;
+            } else if (closestWin[j].includes(option[1])) {
+              output = option[1];
+              break;
+            } else {
+              continue;
+            }
+          }
+        })
+        if (output) return output;
+      }
+
+            
+      let stateCopy = Array.from(state);
+      let picksCopy = Array.from(picks);
+      let picksOppCopy = Array.from(otherPlayer.picks);
+      let bestOption = {}
+      for (i = 0; i < stateCopy.length; i++) {
+  
+        if (stateCopy[i]) continue;
+
+        stateCopy[i] = this.marker;
+        picksCopy.push(i);
+        let currentBest = this.findBestPick(stateCopy, picksCopy);
+        bestOption[currentBest] = ([currentBest] in bestOption) ? ++bestOption[currentBest] : 1;
+        stateCopy[i] = null;
+        picksCopy.pop();
+      }
+
+      let bestOptionArray = Array.from(Object.keys(bestOption)).sort((lastOption, nextOption) => (bestOption[lastOption] < bestOption[nextOption]) ? 1 : -1)
+      return bestOptionArray[0]
     },
     
     getPicks() {
@@ -88,7 +179,7 @@ const game = (function() {
     playerTwo.resetPlayer();
     _createBoard();
     
-    if (!playerOne.isHuman) otherPlayer.pickCPU();
+    if (!playerOne.isHuman) playerOne.pickCPU();
     }
 
     const toggleAIRadio = function() {
@@ -127,7 +218,7 @@ const game = (function() {
     const otherPlayer = (whoPicked == playerOne) ? playerTwo : playerOne;
 
     if (_checkEnd(whoPicked)) {
-      if (!otherPlayer.isHuman) {playerOne.pickCPU()}
+      if (!otherPlayer.isHuman) {otherPlayer.pickCPU()}
     }
   };
   
